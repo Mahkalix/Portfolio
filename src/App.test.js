55728c284cfe3462335bada7ef6b1f32c6a0d3a3
Maxence Badin Léger <maxencelger@gmail.com>
2023-10-16 16:07:44 +0200

    about done
    il reste plus que l'animation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { ThemeProvider } from "./components/ThemeSwitch";
+import { MemoryRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <ThemeProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </ThemeProvider>
+  );
+
+  // Recherchez l'élément par son contenu HTML
+  const linkElement = screen.getByText(/learn react/i, { selector: "a" });
+
   expect(linkElement).toBeInTheDocument();
 });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,23 @@
 import React from "react";
 import ScrollText from "../components/ScrollText";
-import arrow from "../assets/svg/arrow.svg";
+import About from "../components/About.js";
+import Projects from "../components/Projects.js";
+import Hero from "../components/Hero.js";
 
 const Home = () => {
   return (
     <>
       <main>
-        <section className="hero">
-          <h1 className="title">
-            Maxence <br />
-            Badin-Léger
-          </h1>
-          <div className="subtitle">
-            <div className="container-reverse">
-              <div className="item-one">Portfolio</div>
-              <div className="item-two">Portfolio</div>
-            </div>
-            <div className="description">
-              Hello 👋 <br />
-              I'm a front-end developer / web integrator's student <br />
-              based in Grenoble, France.
-            </div>
-          </div>
-        </section>
+        <Hero />
 
         {/* <ScrollText text="WELCOME TO PORTFOLIO - WELCOME TO PORTFOLIO - WELCOME TO PORTFOLIO - WELCOME TO PORTFOLIO - WELCOME TO PORTFOLIO - " />
         <section className="portfolio"></section> */}
-
         <ScrollText text="ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT -" />
-        <section className="about">
-          <h1>
-            GOOD TO SEE YOU ! <br /> LET ME TELL YOU A BIT MORE ABOUT ME
-          </h1>
-          <div className="scroll">
-            Scroll to explore <img src={arrow} alt="arrow" />
-          </div>
-        </section>
+
+        <About />
 
         <ScrollText text="PROJECTs - PROJECTs - PROJECTs - PROJECTs - PROJECTs - PROJECTs - PROJECTs - PROJECTs - PROJECTS - PROJ" />
-        <section className="projects"></section>
+        <Projects />
       </main>
     </>
   );
