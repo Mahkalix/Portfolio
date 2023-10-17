0a6c0a68e041b91a84724227076cdfd77298b757
Maxence Badin Léger <maxencelger@gmail.com>
2023-10-17 04:07:27 +0200

    footer finis
    manque projets a finir

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,28 @@
 import React from "react";
+import githubd from "../assets/svg/githubd.svg";
+import githubl from "../assets/svg/githubl.svg";
+import linkedind from "../assets/svg/linkedind.svg";
+import linkedinl from "../assets/svg/linkedinl.svg";
+import { useTheme } from "../components/ThemeSwitch";
 
 const Footer = () => {
-  return <div>footer</div>;
+  const githubUrl = "https://github.com/Mahkalix";
+  const linkedinUrl = "https://www.linkedin.com/in/maxencebadin-l%C3%A9ger/";
+  const { theme } = useTheme();
+
+  return (
+    <footer>
+      <div className="social-media">
+        <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+          <img src={theme === "light" ? githubd : githubl} alt="Logo" />
+        </a>
+        <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+          <img src={theme === "light" ? linkedind : linkedinl} alt="Logo" />
+        </a>
+      </div>
+      <p>Designed and developed by me©2023 – All Rights Reserved</p>
+    </footer>
+  );
 };
 
 export default Footer;
