45d691368fd8aafacff35f236b486997f55ae954
Maxence Badin Léger <maxencelger@gmail.com>
2023-10-30 15:12:31 +0100

    menu center

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -28,72 +28,74 @@ const Menu = () => {
       <Hamburger toggled={menuOpen} toggle={toggleMenu} />
 
       <ul id="menu-body" className={`menu ${menuOpen ? "open" : "close"}`}>
-        <li className="menu-item">
-          <NavHashLink
-            to="/"
-            onClick={closeMenu}
-            className={location.pathname === "/" ? "active" : ""}
-          >
-            <div className="invisible"> HOME</div>
+        <div className="center">
+          <li className="menu-item">
+            <NavHashLink
+              to="/"
+              onClick={closeMenu}
+              className={location.pathname === "/" ? "active" : ""}
+            >
+              <div className="invisible"> HOME</div>
 
-            <span className="Mask Top">
-              <span>Home</span>
-            </span>
-            <span className="Mask">
-              <span>Home</span>
-            </span>
-          </NavHashLink>
-        </li>
+              <span className="Mask Top">
+                <span>Home</span>
+              </span>
+              <span className="Mask">
+                <span>Home</span>
+              </span>
+            </NavHashLink>
+          </li>
 
-        <li className="menu-item">
-          <NavHashLink
-            to="/#about"
-            onClick={closeMenu}
-            className={location.pathname === "/#about" ? "active" : ""}
-          >
-            <div className="invisible"> ABOUT</div>
+          <li className="menu-item">
+            <NavHashLink
+              to="/#about"
+              onClick={closeMenu}
+              className={location.pathname === "/#about" ? "active" : ""}
+            >
+              <div className="invisible"> ABOUT</div>
 
-            <span className="Mask Top">
-              <span>ABOUT</span>
-            </span>
-            <span className="Mask">
-              <span>ABOUT</span>
-            </span>
-          </NavHashLink>
-        </li>
+              <span className="Mask Top">
+                <span>ABOUT</span>
+              </span>
+              <span className="Mask">
+                <span>ABOUT</span>
+              </span>
+            </NavHashLink>
+          </li>
 
-        <li className="menu-item">
-          <NavHashLink
-            to="/#projects"
-            onClick={closeMenu}
-            className={location.pathname === "/#projects" ? "active" : ""}
-          >
-            <div className="invisible"> PROJECTS</div>
+          <li className="menu-item">
+            <NavHashLink
+              to="/#projects"
+              onClick={closeMenu}
+              className={location.pathname === "/#projects" ? "active" : ""}
+            >
+              <div className="invisible"> PROJECTS</div>
 
-            <span className="Mask Top">
-              <span>PROJECTS</span>
-            </span>
-            <span className="Mask">
-              <span>PROJECTS</span>
-            </span>
-          </NavHashLink>
-        </li>
-        <li className="menu-item">
-          <NavHashLink
-            to="/contact"
-            onClick={closeMenu}
-            className={location.pathname === "/contact" ? "active" : ""}
-          >
-            <div className="invisible"> Contact</div>
+              <span className="Mask Top">
+                <span>PROJECTS</span>
+              </span>
+              <span className="Mask">
+                <span>PROJECTS</span>
+              </span>
+            </NavHashLink>
+          </li>
+          <li className="menu-item">
+            <NavHashLink
+              to="/contact"
+              onClick={closeMenu}
+              className={location.pathname === "/contact" ? "active" : ""}
+            >
+              <div className="invisible"> Contact</div>
 
-            <span className="Mask Top">
-              <span>Contact</span>
-            </span>
-            <span className="Mask">
-              <span>Contact</span>
-            </span>
-          </NavHashLink>
-        </li>
+              <span className="Mask Top">
+                <span>Contact</span>
+              </span>
+              <span className="Mask">
+                <span>Contact</span>
+              </span>
+            </NavHashLink>
+          </li>
+        </div>
       </ul>
     </section>
   );
