0ba9185a75398866bf719ffc9f3b83b1e6ebbf35
Maxence Badin Léger <maxencelger@gmail.com>
2023-11-19 20:27:44 +0100

    contact module

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 import me from "../assets/images/me.webp";
 import ScrollText from "../components/ScrollText";
+import styles from "../styles/about.module.scss";
 
 const About = () => {
   return (
     <>
-      <section id="about">
+      <section id={styles.about}>
         <ScrollText text="ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT -" />
 
-        <div className="about-header">
+        <div className={styles.aboutHeader}>
           <h1>
             GOOD TO SEE YOU ! <br />
             LET ME TELL YOU A BIT MORE ABOUT ME :
           </h1>
         </div>
-        <div className="text">
-          <div className="floating">
-            <img className="normal" src={me} alt="" />
+        <div className={styles.text}>
+          <div className={styles.floating}>
+            <img className={styles.normal} src={me} alt="" />
           </div>
 
-          <div className="introduction">
+          <div className={styles.introduction}>
             <h2>Who am I ?</h2>
 
             <p>
-              <span className="number">01/</span> I'm Max, a 22-year-old web
+              <span className={styles.introduction}>01/</span> I'm Max, a 22-year-old web
               wizard from the cozy town of Saint Martin D'Uriage near Grenoble.
               I've got a knack for turning wild web ideas into reality through a
               blend of code and design. <br />
             </p>
 
             <p>
-              <span className="number">02/</span> My web adventure started with
+              <span className={styles.number}>02/</span> My web adventure started with
               the basics - HTML, CSS, and JavaScript during a nine-month course
               that set me on this path. Along the way, I got a taste of React
               and found myself hooked on creating dynamic web experiences. But
@@ -39,13 +40,13 @@ const About = () => {
               happen. <br />
             </p>
             <p>
-              <span className="number">03/</span> When it comes to my
+              <span className={styles.number}>03/</span> When it comes to my
               personality, I'm all about creative thinking, self-driven
               learning, and being a problem-solving ninja. If you have a digital
               challenge, I'm the one. <br />
             </p>
             <p>
-              <span className="number">04/</span> I wrapped up my formal
+              <span className={styles.number}>04/</span> I wrapped up my formal
               education with a BTEC Higher National Diploma (the French
               equivalent of Bac +2), but the learning never stops in this
               ever-evolving digital realm. So, if you're up for some web
