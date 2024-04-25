a20909eafe0be95270e88606f49cf0e2c8630c96
Maxence Badin Léger <maxencelger@gmail.com>
2024-04-25 10:38:32 +0200

    change picture, spidey

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import me from "../assets/images/me.png";
+import me from "../assets/images/me.jpg";
 import ScrollText from "../components/ScrollText";
 import styles from "../styles/about.module.scss";
 
 const About = () => {
   return (
     <>
-      <section id={styles.about}>
+      <section id="about" className={styles.about}>
         <ScrollText text="ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT - ABOUT -" />
 
         <div className={styles.aboutHeader}>
@@ -24,34 +24,40 @@ const About = () => {
             <h2>Who am I ?</h2>
 
             <p>
-              <span className={styles.number}>01/</span> I'm Max, a 22-year-old web
-              wizard from the cozy town of Saint Martin D'Uriage near Grenoble.
-              I've got a knack for turning wild web ideas into reality through a
-              blend of code and design. <br />
+              <span className={styles.number}>01/</span> I'm Max, a 23-year-old
+              web wizard from the cozy town of Saint Martin d'Uriage near
+              Grenoble. I have a knack for turning wild web ideas into reality
+              through a blend of code and design, and right now, I’m on the
+              lookout for my next adventure. <br />
             </p>
 
             <p>
-              <span className={styles.number}>02/</span> My web adventure started with
-              the basics - HTML, CSS, and JavaScript during a nine-month course
-              that set me on this path. Along the way, I got a taste of React
-              and found myself hooked on creating dynamic web experiences. But
-              hey, I didn't stop there. I also decided to dive into the design
-              world because, let's be honest, it's where all the cool visuals
-              happen. <br />
+              <span className={styles.number}>02/</span>My web journey kicked
+              off with the essentials—HTML, CSS, and JavaScript—during a
+              nine-month course that truly ignited my passion. Along the way, I
+              dove into React and became hooked on building dynamic web
+              experiences. I didn’t stop there, though! I’ve also ventured into
+              the design world because, let’s be honest, that’s where all the
+              cool visuals happen.
+              <br />
             </p>
             <p>
-              <span className={styles.number}>03/</span> When it comes to my
-              personality, I'm all about creative thinking, self-driven
-              learning, and being a problem-solving ninja. If you have a digital
-              challenge, I'm the one. <br />
+              <span className={styles.number}>03/</span> Currently, I’m studying
+              for my BUT Métiers du Multimédia et de l’Internet at IUT 1
+              Grenoble, where I’m deepening my skills in backend development
+              with PHP and Laravel. Additionally, I’m exploring Java and Android
+              Studio, and soon, I’ll be getting hands-on with Next.js and Docker
+              as part of my training. Every new tool I learn opens up exciting
+              possibilities, and I’m always eager to apply them to real-world
+              projects. <br />
             </p>
             <p>
-              <span className={styles.number}>04/</span> I wrapped up my formal
-              education with a BTEC Higher National Diploma (the French
-              equivalent of Bac +2), but the learning never stops in this
-              ever-evolving digital realm. So, if you're up for some web
-              adventures or just want to chat about the latest web trends, hit
-              me up!
+              <span className={styles.number}>04/</span> Although I’ve already
+              earned a BTEC Higher National Diploma (the French equivalent of
+              Bac +2), I believe that learning is a never-ending
+              journey—especially in the fast-moving world of web development. If
+              you think I’d be a good fit for your team, or just want to chat
+              about the latest tech trends, feel free to reach out!
             </p>
           </div>
         </div>
